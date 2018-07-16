d505bcd11072aaced039fe1448dc864ff32d289c
techemayo <muhammadasifmayo@gmail.com>
2018-07-16 10:09:26 +0500

    Model Schema added for Products and WishList

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var db = mongoose.connect("mongodb://localhost/swag-shop");
 
+var Product = require("./model/product");
+var WishList = require("./model/wishlist"); 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.listen(3000,function(){
    console.log("Swag shop API running on port 3000,,,"); 
 });
+ 
