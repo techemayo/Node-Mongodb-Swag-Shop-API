0cb3bb262a43e67dc22772a1738f57752f6b241f
techemayo <muhammadasifmayo@gmail.com>
2018-07-16 15:33:42 +0500

    Wish List title type error fix

--- model/wishlist.js
+++ model/wishlist.js
@@ -3,7 +3,7 @@ var Schema = mongoose.Schema;
 var ObjectId = mongoose.Schema.Types.ObjectId;
 
 var wishList = new Schema({
-    title : {String, default: "Cool Wish List"},
+    title : { type: String, default: "Cool Wish List"},
     products : [{type: ObjectId, ref: "Product"}]
 });
 
