e1ae5c49cc69fdb408e26ad5d0a3a3f64495697b
techemayo <muhammadasifmayo@gmail.com>
2018-07-16 15:36:24 +0500

    product posting functionality added

--- server.js
+++ server.js
@@ -10,6 +10,19 @@ var WishList = require("./model/wishlist");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
+app.post("/product", function( request, response ){
+   var product = new Product();
+    product.title = request.body.title;
+    product.price = request.body.price;
+    product.save(function(err, savedProduct){
+       if(err){
+           response.status(500).send({error:"Could not save product"});
+       } else{
+           response.send(savedProduct);
+       }
+    });
+});
+
 app.listen(3000,function(){
    console.log("Swag shop API running on port 3000,,,"); 
 });
